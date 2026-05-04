4f2523d3c619d59b8be1f42aa1637c468ec61e2e
Vijay <hsvijay264737@gmail.com>
2026-05-04 23:04:41 +0530

    Add files via upload

--- Day22.py
+++ Day22.py
@@ -1,15 +1,44 @@
-def isAnagram(s, t):
-    return sorted(s) == sorted(t)
+def rotate_image(matrix):
+    n = len(matrix)
+
+    
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    
+    for row in matrix:
+        row.reverse()
+
+    return matrix
 
 
-s = input("Enter first string: ")
-t = input("Enter second string: ")
 
-result = isAnagram(s, t)
+n = int(input("Enter size of matrix (n x n): "))
 
-print("Output:", result)
+print("Enter matrix row by row (space separated):")
+matrix = []
+for _ in range(n):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+
+
+result = rotate_image(matrix)
+
+
+print("Rotated Matrix:")
+for row in result:
+    print(*row)
+
 
 #sample output
-#Enter first string: anagram
-#Enter second string: nagaram
-#Output: True
+#Enter size of matrix (n x n): 3
+#Enter matrix row by row (space separated):
+#1 2 3
+#4 5 6
+#7 8 9
+#Rotated Matrix:
+#7 4 1
+#8 5 2
+#9 6 3
+
